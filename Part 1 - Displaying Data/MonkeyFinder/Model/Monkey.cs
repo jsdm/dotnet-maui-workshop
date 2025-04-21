b1b5f39218f811ab7262df8b28f8ce57d1a82d96
JSDM <161076@gmail.com>
2025-04-22 09:23:41 +1000

    First part

--- Part 1 - Displaying Data/MonkeyFinder/Model/Monkey.cs
+++ Part 1 - Displaying Data/MonkeyFinder/Model/Monkey.cs
@@ -4,4 +4,24 @@ namespace MonkeyFinder.Model;
 
 public class Monkey
 {
+    [JsonPropertyName("Name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("Location")]
+    public string Location { get; set; }
+
+    [JsonPropertyName("Details")]
+    public string Details { get; set; }
+
+    [JsonPropertyName("Image")]
+    public string Image { get; set; }
+
+    [JsonPropertyName("Population")]
+    public int Population { get; set; }
+
+    [JsonPropertyName("Latitude")]
+    public double Latitude { get; set; }
+
+    [JsonPropertyName("Longitude")]
+    public double Longitude { get; set; }
 }
