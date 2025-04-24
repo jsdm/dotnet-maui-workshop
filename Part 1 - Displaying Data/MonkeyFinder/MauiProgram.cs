533f0e643145ee582ea0d991d0b4035180204fac
JSDM <161076@gmail.com>
2025-04-24 15:52:24 +1000

    Do part 1

--- Part 1 - Displaying Data/MonkeyFinder/MauiProgram.cs
+++ Part 1 - Displaying Data/MonkeyFinder/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using MonkeyFinder.Services;
 using MonkeyFinder.View;
 
 namespace MonkeyFinder;
@@ -14,10 +15,13 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 			});
-
-#if DEBUG
-		builder.Logging.AddDebug();
-#endif
+		
+        builder.Services.AddSingleton<MonkeyService>();
+        builder.Services.AddSingleton<MonkeysViewModel>();
+		builder.Services.AddSingleton<MainPage>();
+//#if DEBUG
+//        builder.Logging.AddDebug();
+//#endif
 
 		
 
--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeysViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeysViewModel.cs
@@ -1,5 +1,41 @@
-﻿namespace MonkeyFinder.ViewModel;
+﻿using MonkeyFinder.Services;
+namespace MonkeyFinder.ViewModel;
 
 public partial class MonkeysViewModel : BaseViewModel
 {
+    MonkeyService monkeyService;
+    public ObservableCollection<Monkey> Monkeys { get; } = new();
+
+    public MonkeysViewModel(MonkeyService monkeyService)
+    {
+        Title = "Monkeys";
+        this.monkeyService = monkeyService;
+    }
+
+    [RelayCommand]
+    async Task GetMonkeysAsync()
+    {
+        if (IsBusy)
+            return;
+        try
+        {
+            IsBusy = true;
+            var monkeys = await monkeyService.GetMonkeys();
+            if (Monkeys.Count != 0)
+                Monkeys.Clear();
+
+            foreach (var monkey in monkeys)
+            {
+                Monkeys.Add(monkey);
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"Unable to get monkeys: {ex.Message}");
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
 }
